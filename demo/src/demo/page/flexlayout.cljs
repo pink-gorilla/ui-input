0f0cf974a3aa7741065496412db1b23df797516f
awb99 <awb99@gmail.com>
2024-10-24 16:38:30 -0500

    flexlayout custom component multimethod

--- demo/src/demo/page/flexlayout.cljs
+++ demo/src/demo/page/flexlayout.cljs
@@ -1,11 +1,12 @@
 (ns demo.page.flexlayout
   (:require
-   [ui.flexlayout :refer [layout]]))
+   [ui.flexlayout :refer [create-model layout add-node]]))
 
 (def model
   {:global {:tabEnableRename false
             :tabEnableClose false
             :tabEnableFloat true
+            :tabSetEnableActiveIcon true
             }
    :layout {:type "row"
             :weight 100
@@ -13,17 +14,26 @@
                         :weight 50
                         :children [{:type "tab"
                                     :name "One"
-                                    :component "button"}]}
+                                    :component "button"
+                                    :enableClose false,
+                                    :icon "/r/images/bar_chart.svg"
+                                    }]}
                        {:type "tabset"
                         :weight 100
-                        :selected 0
+                        :selected 1
                         :children
                         [{:type "tab"
                           :name "One"
-                          :component "panel"}
+                          :component "url"
+                          :icon "/r/images/add.svg"
+                          :helpText "this tab has helpText defined"
+                          :id "50"}
                          {:type "tab"
                           :name "two"
-                          :component "panel"}
+                          :component "panel"
+                          :icon "/r/images/article.svg"
+                          :id "100"
+                          }
                          {:type "tab"
                           :name "3"
                           :component "panel"}
@@ -43,7 +53,67 @@
                           :component "grid",
                           :enableClose false}]}]})
 
-(defn flex-layout-page [{:keys [route-params query-params handler] :as route}]
-  [:div.h-screen.w-screen.bg-blue-100.
-   [layout model]])
+(def m (create-model
+       {:model model
+        :options {"50" "https://en.wikipedia.org/wiki/Main_Page"
+                  "100" 10000
+                  }}))
 
+(set! (.-model js/window) m)
+
+(defn flex-layout-page [{:keys [route-params query-params handler] :as route}]
+  [:div.h-screen.w-screen
+   {:style {:display "flex"
+            :flex-direction "column"
+            :flex-grow 1}}
+     [:div {:dir "ltr"
+                  :style {:margin "2px"
+                          :display "flex"
+                          :align-items "center"
+                          }}
+     [:button
+      {:on-click #(add-node m {:component "grid"
+                               :icon "/r/images/article.svg",
+                               :name "Grid-added"})
+       :style {:border-radius "5px"
+               :border "1px solid lightgray"}}
+      "add node"
+      ]
+      
+      [:button
+       {:on-click #(add-node m {:component "url"
+                                :icon "/r/images/article.svg",
+                                :name "duck"
+                                :options "https://kibot.com"
+                                :id "duck1"
+                                })
+        :style {:border-radius "5px"
+                :border "1px solid lightgray"}}
+       "add duck"]
+      
+      ]
+   [:div {:style {:display "flex"
+                  :flex-grow "1"
+                  :position "relative"
+                  :border "1px solid #ddd"}}
+    [layout m]
+    ]
+   ])
+
+
+ ; if (node.getExtraData().data == null) {
+; // create data in node extra data first time accessed
+;                node.getExtraData().data = this.makeFakeData();
+;            }
+
+ ;const config = node.getConfig();
+;                ;if (config.type === "url") {
+;                    return <iframe title={node.getId()} src={config.data} style={{ display: "block", border: "none", boxSizing: "border-box" }} width="100%" height="100%" />;
+;                } else if (config.type === "html") {
+;                    return (<div dangerouslySetInnerHTML={{ __html: config.data }} />);
+;                } else if (config.type === "text") {
+;                    return (
+;                        <textarea style={{ position: "absolute", width: "100%", height: "100%", resize: "none", boxSizing: "border-box", border: "none" }}
+;                            defaultValue={config.data}
+;                        />);
+;                }
--- src/ui/flexlayout.cljs
+++ src/ui/flexlayout.cljs
@@ -6,31 +6,16 @@
 
 ;; https://www.npmjs.com/package/flexlayout-react
 ;; 20k weekly downloads from npm
-; https://github.com/caplin/FlexLayout/blob/master/examples/demo/App.tsx
 
 ;; see:
+
+;; official? demo
+;; https://github.com/caplin/FlexLayout/blob/master/examples/demo/App.tsx
+
+;; clojurescript example
 ;; https://github.com/dundalek/daba/blob/master/components/core/src/io/github/dundalek/daba/ui/viewers/root_docking.cljs
 
-(defn create-model [model]
-  (-> model
-      clj->js
-      (Model.fromJson)))
-
-(def factory (fn [^js node]
-               (let [cell-id (.getId node)]
-                 (r/as-element
-        ;[cell-panel {:!value (ratom/make-reaction
-        ;                       (fn [] (get @!cells cell-id)))
-        ;             :cell-id cell-id}]
-                  [:div "I am cell: " (str cell-id)]))))
-
-(def handle-action (fn [^js action]
-                    ; (if (= FlexLayout.Actions.DELETE_TAB (.-type action))
-                    ;   (let [cell-id (-> action .-data .-node)]
-                    ;     (println "cell deleted: " cell-id)   
-                    ;     js/undefined)
-                    ;   action
-                     action))
+;; https://github.com/openworm/geppetto-client/tree/development/geppetto-ui/src/flex-layout/src
 
 ;(let [^js tabset-node
   ;        (tabset-with-most-children model)
@@ -44,6 +29,8 @@
 
 ; cell-id (.getId node)
 
+;  attributeDefinitions.add("config", undefined).setType(Attribute.JSON);
+
 (defn tab-button-title [{:keys [cell-id]}]
   [:span (str cell-id)])
 
@@ -54,14 +41,111 @@
                                        ;         (fn [] cell-id))
                                            :cell-id cell-id}]))))
 
+;; model.addChangeListener(onModelChange);
+;;  model.removeChangeListener(onModelChange);
+; doAction
+; model.getActiveTabset
+
+;; model.toJson ()
+
+;;  const jsonText = JSON.stringify(model.toJson (), null, "\t");}
+
+;; actions
+; ADD_NODE
+; ADJUST_BORDER_SPLIT
+; ADJUST_WEIGHTS
+; CLOSE_WINDOW
+; CREATE_WINDOW
+; DELETE_TAB
+; DELETE_TABSET
+; MAXIMIZE_TOGGLE
+; MOVE_NODE
+; POPOUT_TAB
+; POPOUT_TABSET
+; RENAME_TAB
+; SELECT_TAB
+; SET_ACTIVE_TABSET
+; UPDATE_MODEL_ATTRIBUTES
+
+(def handle-action (fn [^js action]
+                    ; (if (= FlexLayout.Actions.DELETE_TAB (.-type action))
+                    ;   (let [cell-id (-> action .-data .-node)]
+                    ;     (println "cell deleted: " cell-id)   
+                    ;     js/undefined)
+                    ;   action
+                     action))
+
+(defn add-node [{:keys [layout option-a]} {:keys [id options] :as node}]
+  (when (and layout @layout)
+    (println "new node: " id " options: " options)
+    (when (and options id)
+      (swap! option-a assoc id options))
+    (println "adding new node to layout..")
+    (.addTabToActiveTabSet
+     @layout
+     (clj->js node))))
+
+(defmulti component-ui  (fn [e] (:component e)))
+
+(defmethod component-ui :default [{:keys [component
+                                          id
+                                          options]}]
+  [:div "I am node id: " (str id)
+   "component: " component
+   "options: " (pr-str options)])
+
+(defmethod component-ui "url" [{:keys [id options]}]
+  ;[:div.bg-blue-100 "url: " (str options) "..."
+  [:iframe {:title id
+            :src options
+            :style {:display "block"
+                    :border "none"
+                    :boxSizing "border-box"}
+            :width "100%"
+            :height "100%"}]
+;   ]
+  )
+
+(defmethod component-ui "panel" [{:keys [id options]}]
+  [:div.bg-red-300.w-full.h-full
+   "I am panel id: " (str id)
+   "options: " (pr-str options)])
+
+(defn make-factory [option-a]
+  (fn [^js node]
+    (let [cell-id (.getId node)
+          component (.getComponent node)
+          option (get @option-a cell-id)
+          comp (component-ui {:options option
+                              :id cell-id
+                              :component component})]
+      (println "cell: " cell-id "option: " option)
+      (r/as-element
+       comp
+        ;[cell-panel {:!value (ratom/make-reaction
+        ;                       (fn [] (get @!cells cell-id)))
+        ;             :cell-id cell-id}]
+       ))))
+;node. getConfig():
+
+(defn create-model [{:keys [model options]
+                     :or {options {}}}]
+  (let [layout (clojure.core/atom nil)
+        option-a (clojure.core/atom options)]
+    {:model (-> model
+                clj->js
+                (Model.fromJson))
+     :layout layout ; react-ref goes here
+     :ref (fn [el]
+            (reset! layout el))
+     :option-a option-a
+     :factory (make-factory option-a)
+     :titleFactory title-factory
+     :onAction handle-action}))
+
 (defn layout
   "The model is tree of Node objects that define the structure of the layout.
-   The factory is a function that takes a Node object and returns a React componen
-   "
-  [model]
-  [:> Layout
-   {:model (create-model model)
-    :factory factory
-    :titleFactory title-factory
-    :onAction handle-action}])
+   The factory is a function that takes a Node object and returns a React componen"
+  [spec]
+  [:> Layout spec])
 
